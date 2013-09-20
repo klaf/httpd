7e47ceaadca6b3400c9b641266854f0d13834a53
petermalone <peter@petermalone.org>
2013-09-20 18:47:51 +0100

    cleaned comments

--- httpd.c
+++ httpd.c
@@ -1,18 +1,18 @@
 /*  mini-httpd v0.1
- *   *  Tiny httpd written in C for Linux.
- *    *  Copyright (C) 2013  Peter Malone
- *     *  Licensed under GPLv2. See the LICENSE file for details.
- *      * 
- *       * 
- *        *  TODO LIST:
- *         *          1. Clean code and modularize
- *          *          2. Add logger
- *           *          3. Add threading
- *            *          4. Add signals
- *             *          5. Add HTTP POST
- *              *          6. Secure, secure, secure!
- *               * 
- *                */
+ *  Tiny httpd written in C for Linux.
+ *  Copyright (C) 2013  Peter Malone
+ *  Licensed under GPLv2. See the LICENSE file for details.
+ * 
+ * 
+ *  TODO LIST:
+ *          1. Clean code and modularize
+ *          2. Add logger
+ *          3. Add threading
+ *          4. Add signals
+ *          5. Add HTTP POST
+ *          6. Secure, secure, secure!
+ *
+ */
 
 #include "includes.h"
 #include "config.h"
